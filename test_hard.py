eb38ad17914692a5ebb4ba5af9bcdc2639706b33
James Ji <jamesx.ji@gmail.com>
2022-01-03 19:04:25 +1100

    readding some tests back into test_hard.py

--- test_hard.py
+++ test_hard.py
@@ -11,6 +11,7 @@ def test_empty():
 
 def test_single():
     assert is_unlocked(["MATH1081"], "COMP3153") == True
+    assert is_unlocked(["ELEC2141"], "COMP3211") == True
     assert is_unlocked(["COMP1511", "COMP1521", "COMP1531"], "COMP3153") == False
 
 def test_compound():
@@ -21,3 +22,16 @@ def test_compound():
 def test_simple_uoc():
     assert is_unlocked(["COMP1511", "COMP1521", "COMP1531", "COMP2521"], "COMP4161") == True
     assert is_unlocked(["COMP1511", "COMP1521"], "COMP4161") == False
+
+def test_annoying_uoc():
+    assert is_unlocked(["COMP9417", "COMP9418", "COMP9447"], "COMP9491") == True
+    assert is_unlocked(["COMP6441"], "COMP9302") == False
+    assert is_unlocked(["COMP6441", "COMP64443", "COMP6843", "COMP6445"], "COMP9302") == True
+    assert is_unlocked(["COMP1234", "COMP5634", "COMP4834"], "COMP9491") == False
+    assert is_unlocked(["COMP3901"], "COMP3902") == False
+    assert is_unlocked(["COMP3901", "COMP6441", "COMP6443"], "COMP3902") == False
+    assert is_unlocked(["COMP3901", "COMP3441", "COMP3443"], "COMP3902") == True
+
+def test_cross_discipline():
+    assert is_unlocked(["COMP1911", "MTRN2500"], "COMP2121") == True
+    assert is_unlocked(["COMP1521"], "COMP2121") == True
