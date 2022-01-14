b8042a14167cb4fdfa037660ca31bb6ebc61f422
theoa <theo.ang816@gmail.com>
2022-01-14 15:57:08 +0700

    hard.py new version

--- hard.py
+++ hard.py
@@ -23,8 +23,93 @@ with open("./conditions.json") as f:
     CONDITIONS = json.load(f)
     f.close()
     
-# Just realized that it was probably intended for us to use CONDITIONS here (never worked w JSON before so just ignored it lol)
-# I'll try think up and put up another solution using it in another branch
+#####################################
+# String formatting functions
+
+def rmv_xtra_space(string):         # Remove random spaces
+    string = string.split()
+    string = ' '.join(string)
+    return string
+
+def rmv_fullstop(string):           # Remove fullstops at the end
+    if string[-1] == '.':
+        string = string[:-1]
+    return string
+
+def convert_or(string):             # Convert all "or"s to "OR"s
+    for i in range(len(string) - 1):
+        if string[i:i+2] == 'or':
+            string = string[0:i] + "OR" + string[i+2:]
+    return string
+
+def convert_and(string):            # Convert all "and"s to "AND"s
+    for i in range(len(string) - 2):
+        if string[i:i+3] == 'and':
+            string = string[0:i] + "AND" + string[i+3:]
+    return string
+
+#####################################
+# HELPER FUNCTIONS
+
+def convert_uoc(prereq_arr):        # Converts all uoc prerequisites into a certain format below
+    i = 0; j = 1                    #  uoc | prereq
+    while i < len(prereq_arr):
+        
+        if prereq_arr[i].isnumeric():
+            uoc = prereq_arr[i]
+            
+            while  (i+j < len(prereq_arr) and
+                    not prereq_arr[i+j].isnumeric() and 
+                    prereq_arr[i+j] != "COMP" and
+                    not prereq_arr[i+j][0] == "("):
+
+                j += 1
+            
+            if i+j >= len(prereq_arr):   # Completion of ___ units of credit
+                uoc = uoc + "|" + ""
+            elif prereq_arr[i+j][0] == "(":    # Completion of ___ units of credit in (courses)
+                uoc = uoc + "|" + prereq_arr[i+5][1:-1]
+                j += 1
+            elif prereq_arr[i+j] == "COMP":     # Completion of ___ units of credit in COMP courses
+                uoc = uoc + "|" + "COMP"
+                j += 2
+            elif prereq_arr[i+j].isnumeric():     # Completion of ___ units of credit in level _ COMP courses
+                uoc = uoc + "|" + "COMP" + str(prereq_arr[i+j])
+                j += 3
+            
+            prereq_arr = prereq_arr[:i] + [uoc] + prereq_arr[i+j:]
+            j = 1
+            
+        i += 1
+    
+    return prereq_arr
+
+def in_course_list(course, course_list):        # Checks if a course is in the course_list
+    if course in course_list:
+        return True
+    return False
+
+def eval (uoc, prereq, course_list):            # Handles the uoc requirements
+    
+    uoc = int(uoc) / 6; prereq = prereq.split(", ")
+    if len(prereq) > 1:        # if prereq was a list
+        intersect = set(course_list) & set(prereq)
+
+        if len(intersect) >= uoc:
+            return True
+        
+    else:       # if prereq was of the form ["COMP1"] for lvl 1 courses etc
+        count = 0; course_type = prereq[0]; 
+        for course in course_list:
+            if course_type in course:
+                count += 1
+            
+        if count >= uoc:
+            return True
+        
+    return False
+
+#####################################
 
 def is_unlocked(course_list, target_course):
     """Given a list of course codes a student has taken, return true if the target_course 
@@ -35,101 +120,86 @@ def is_unlocked(course_list, target_course):
 
     You can assume all courses are worth 6 units of credit
     """
+    if target_course == "COMP1511":     # Special Case
+        return True
 
-    def eval (uoc, courses):        # Checks whether the UOC condition has been met
+    if len(target_course) != 8:         # Recursive
+        string = target_course
+    else:
+        string = CONDITIONS[target_course]      # First time
 
-        if len(courses) > 1:        # if courses was a list
-            intersect = set(course_list) & set(courses)
-            if len(intersect) >= uoc:
-                return intersect.pop()
+    string = rmv_xtra_space(string)
+    string = rmv_fullstop(string)
+    string = convert_or(string)
+    string = convert_and(string)
+    
+    condition_arr = []
+    prereq_arr = string.split()
 
-        else:       # if courses was of the form ["COMP1"] for lvl 1 courses etc
-            count = 0; course_type = courses[0]; 
-            for course in course_list:
-                if course_type in course:
-                    count += 1
-                    sample = course
-            
-            if count >= uoc:
-                return sample
+    i = 0; j = 0; open_bracket = -1; end = False
+
+    # Makes anything in brackets into one element AND 
+    # Copies all "AND"s / "OR"s into condition_arr
+
+    while i < len(prereq_arr):
+
+        if prereq_arr[i][0] == "(":
+            while i+j < len(prereq_arr):
+                
+                # Checks to see which ")" is the matching one to the "(" found
+                # In case of multiple brackets e.g. (COMP1511 AND (COMP1521))
+
+                prereq = prereq_arr[i+j]
+                for char in prereq:
+
+                    if char == "(":
+                        open_bracket += 1
+                    elif char == ")":
+                        if open_bracket == 0:
+                            end = True
+                        else:
+                            open_bracket -= 1
+
+                if end:
+                    break
+                j += 1
+
+            prereq_arr[i] = " ".join(prereq_arr[i:i+j+1])
+            prereq_arr = prereq_arr[:i+1] + prereq_arr[i+j+1:]
+            j = 0; open_bracket = -1; end = False
         
-        return "UOC NOT MET"
+        elif prereq_arr[i] == "AND" or prereq_arr[i] == "OR":
+            condition_arr.append(prereq_arr[i])
+            prereq_arr.remove(prereq_arr[i])
+            i -= 1
+
+        i += 1
     
-    """
-    Dic Format Example  -    "Course": [[Arr1], [Arr2], final_condition]
+    prereq_arr = convert_uoc(prereq_arr)
+    
+    i = 0
+    while i < len(prereq_arr):
+        course_type  = prereq_arr[i][:4]
+        
+        if course_type == "COMP" or course_type == "MTRN" or course_type == "DPST" or course_type == "MATH" or course_type == "ELEC":
+            prereq_arr[i] = in_course_list(prereq_arr[i], course_list)
+        elif course_type[0] == "(":                                             # Bracketed element
+            prereq_arr[i] = is_unlocked(course_list, prereq_arr[i][1:-1])
+        elif course_type[0].isnumeric():                                        # UOC prereq element
+            temp = prereq_arr[i].split("|")
+            prereq_arr[i] = eval(temp[0], temp[1], course_list)
+        else:                                                                   # Irrelevant words e.g. Condition of, Prerequisite:
+            prereq_arr.remove(prereq_arr[i])
+            i -= 1
+        
+        i += 1
+    
+    # len(condition_arr) == len(prereq_arr) + 1     ALWAYS
 
-    For each Array within dic["Course"], if the last element was OR, check whether
-    any one of the courses within the Array exists in course_list
-
-    if the last element was AND, check whether the Array (without the AND) is a 
-    subset of course_list
-
-    For Arrays containing the eval function, the eval functioned returned a course
-    present in course_list if the UOC condition was met (TRUE), 
-    and "UOC NOT MET" if not (FALSE). This would provide the same desired output
-    when combined with the AND or OR in the arrays
-
-    For some conditions like that of COMP3151, I modified slightly to fit my format
-    COMP3151: COMP1927 OR ((COMP1521 OR DPST1092) AND COMP2521)     -->
-    COMP3151: COMP1927 OR (COMP1521 AND COMP2521) OR (DPST1092 AND COMP2521)
-    """
-
-    dic = {     
-        "COMP1511": [["AND"], "OR"],
-        "COMP1521": [["COMP1511", "DPST1091", "COMP1911", "COMP1917", "OR"], "OR"],
-        "COMP1531": [["COMP1511", "DPST1091", "COMP1917", "COMP1921", "OR"], "OR"],
-        "COMP2041": [["COMP1511", "DPST1091", "COMP1917", "COMP1921", "OR"], "OR"],
-        "COMP2111": [["MATH1081", "OR"], ["COMP1511", "DPST1091", "COMP1921", "COMP1917", "OR"], "AND"],
-        "COMP2121": [["COMP1917", "COMP1921", "COMP1511", "DPST1091", "COMP1521", "DPST1092", "OR"], ["COMP1911", "MTRN2500", "AND"], "OR"],
-        "COMP2511": [["COMP1531", "OR"], ["COMP2521", "COMP1927", "OR"], "AND"],
-        "COMP2521": [["COMP1511", "DPST1091", "COMP1917", "COMP1921", "OR"], "OR"],
-        "COMP3121": [["COMP1927", "COMP2521", "OR"], "OR"],
-        "COMP3131": [["COMP2511", "COMP2911", "OR"], "OR"],
-        "COMP3141": [["COMP1927", "COMP2521", "OR"], "OR"],
-        "COMP3151": [["COMP1927", "OR"], ["COMP1521", "COMP2521", "AND"], ["DPST1092", "COMP2521", "AND"], "OR"],
-        "COMP3153": [["MATH1081", "OR"], "OR"],
-        "COMP3161": [["COMP1927", "COMP2521", "OR"], "OR"],
-        "COMP3211": [["COMP3222", "ELEC2141", "OR"], "OR"],
-        "COMP3900": [["COMP1531", "OR"], ["COMP2521", "COMP1927", "OR"], [eval(102 / 6, [""]), "OR"], "AND"],
-        "COMP3901": [[eval(12 / 6, ["COMP1"]), "OR"], [eval(18 / 6, ["COMP2"]), "OR"], "AND"],
-        "COMP3902": [["COMP3901", "OR"], [eval(12 / 6, ["COMP3"]), "OR"], "AND"],
-        "COMP4121": [["COMP3121", "COMP3821", "OR"], "OR"],
-        "COMP4128": [["COMP3821", "OR"], ["COMP3121", eval(12 / 6, ["COMP3"]), "AND"], "OR"],
-        "COMP4141": [["MATH1081", "OR"], ["COMP1927", "COMP2521", "OR"], "AND"],
-        "COMP4161": [[eval(18 / 6, [""]), "OR"], "OR"],
-        "COMP4336": [["COMP3331", "OR"], "OR"],
-        "COMP4418": [["COMP3411", "OR"], "OR"],
-        "COMP4601": [["COMP2511", "COMP2911", "OR"], [eval(24 / 6, [""]), "OR"], "AND"],
-        "COMP4951": [[eval(36 / 6, ["COMP"]), "OR"], "OR"],
-        "COMP4952": [["COMP4951", "OR"], "OR"],
-        "COMP4953": [["COMP4952", "OR"], "OR"],
-        "COMP9301": [[eval(12 / 6, ["COMP6443", "COMP6843", "COMP6445", "COMP6845", "COMP6447"]), "OR"], "OR"],
-        "COMP9302": [["COMP6441", "COMP6841", "OR"], [eval(12 / 6, ["COMP6443", "COMP6843", "COMP6445", "COMP6845", "COMP6447"]), "OR"], "AND"],
-        "COMP9417": [["MATH1081", "OR"], ["COMP1531", "COMP2041", "COMP1927", "COMP2521", "OR"], "AND"],
-        "COMP9418": [["MATH5836", "COMP9417", "OR"], "OR"],
-        "COMP9444": [["COMP1927", "COMP2521", "MTRN3500", "OR"], "OR"],
-        "COMP9447": [["COMP6441", "COMP6841", "COMP3441", "OR"], "OR"],
-        "COMP9491": [[eval(18 / 6, ["COMP9417", "COMP9418", "COMP9444", "COMP9447"]), "OR"], "OR"]
-    }
-
-    prereq = dic[target_course]
-    truth_table = []
-    final_condition = prereq.pop()
-
-    for arr in prereq:
-        condition = arr.pop()
+    for condition in condition_arr:
         if condition == "OR":
-            truth_table.append(not set(arr).isdisjoint(course_list))
-        elif condition == "AND":
-            truth_table.append(set(arr) <= set(course_list))
+            prereq_arr = [prereq_arr[0] or prereq_arr[1]] + prereq_arr[2:]
+        if condition == "AND":
+            prereq_arr = [prereq_arr[0] and prereq_arr[1]] + prereq_arr[2:]
     
-    if final_condition == "AND":
-        if False not in truth_table:
-            return True
-        else:
-            return False
-    elif final_condition == "OR":
-        if True in truth_table:
-            return True
-        else:
-            return False
+    return prereq_arr[0]
